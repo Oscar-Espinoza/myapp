ecacc41ff11fdb423b0f0d8ba93243fb7a7efe50
Oscar Espinoza <oscardavid2094@gmail.com>
2019-10-29 17:26:37 -0300

    CARRUSEL TERMINADO (CASI)

--- client/src/components/carousel.js
+++ client/src/components/carousel.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import cities from './cityImages.js'
+import { Gallery, GalleryImage } from "react-gesture-gallery"
 
 const Carousel = () => {
+
+    const [index, setIndex] = useState(0)
+
     return (
-        <div className="bg-dark">
-            <button>back</button>
-            <img src="" alt="" />
-            <button>next</button>
+        <div className="row h-100">
+            <Gallery 
+                index={index}
+                onRequestChange={i => {
+                    setIndex(i);
+                }}>
+                {cities.map(images => (
+                    <div className="col-12">
+                        <div className="row h-100 justify-content-center">
+                            {images.map(image => (
+                                <div className="col-6"><div className="position-absolute bg-white cityName"><h4>{image.name}</h4></div><GalleryImage style={{}} src={image.url}/></div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </Gallery>
+
         </div>
     )
 }
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -16,16 +16,18 @@ class Home extends Component {
   }
   render() {
     return (
-      <div className="row mx-auto" >
+      <div className="row mx-auto w-100 h-100" >
         <Header srcLogo={HeaderLogo} />
-        <div className="col-12 mt-5">
-          <p className="text-center mx-auto">Find your perfect trick, designed by insiders who know and love their cities </p>
+        <div className="col-12">
+          
         </div>
-        <div className="col-12 text-center mt-1" >
+        <div className="col-12 text-center" >
+        <p className="text-center mx-auto">Find your perfect trick, designed by insiders who know and love their cities </p>
           <Link to='/cities'><img src={ArrowLogo} className="arrowLogo" alt="arrowLogo"></img></Link>
-          <p className="pt-4 text-left"> Popular MYitineraries:</p>
+          
         </div>
         <div className="col-12">
+        <p className="text-left"> Popular MYitineraries:</p>
           <Carousel />
         </div>
       </div>
